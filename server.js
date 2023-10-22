568c71b3db52c62904b4ec9da1b1dc81b02a0cd5
SaiBharath-Bungee <saibharath@bungeetech.com>
2023-10-22 17:50:32 +0530

    updated db config and query

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const aurora = require('./lib/dbResources.js');
 
 app.get('/job-updates', async (req, res) =>{
     try {
-        let queryOutput = await aurora.query('select * from jobs');
-        return res.status(200).send(queryOutput);
+        let queryOutput = await aurora.query('select * from jobs where active order by date desc');
+        return res.status(200).send(queryOutput.rows);
     } catch(err) {
         console.log(err);
         return res.status(500).send(err);
